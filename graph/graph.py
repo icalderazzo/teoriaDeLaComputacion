8baeb2aaa718ec36541716ee339e83a0a61131b8
icalderazzo <ignaciocalderazzo@gmail.com>
2022-12-03 21:40:27 -0300

    add scc operations

--- graph/graph.py
+++ graph/graph.py
@@ -1,10 +1,10 @@
-#
+# Adyacencia
 class Adjacency:
     def __init__(self, dest):
         self.dest = dest
         self.value = self.dest.value
 
-# 
+# Nodo o vertice
 class Node:
     def __init__(self, value, visited = False):
         self.value = value
@@ -21,7 +21,7 @@ class Node:
                 return a
         return None             
 
-#
+# Arista
 class Edge:
     def __init__(self, origin, destination):
         self.origin = origin
@@ -32,7 +32,7 @@ class EdgeList:
     def __init__(self, edgeList):
         self.edgeList = edgeList
 
-#
+# Grafo (no dirigido)
 class Graph:
     def __init__(self, nodes, edges = []):
         self.nodes = {}
@@ -49,10 +49,54 @@ class Graph:
             node2 = self._get_node(edge.destination)
             # agrega ambas referencias
             node1.add_adj(node2)
-            node2.add_adj(node1)    
+            node2.add_adj(node1)
+
+    # Componentes conexos
+    def connected_components(self):
+        # 1: Resetear visitados
+        # 2: Realizar busqueda en profundidad (DFS) en aquellos nodos que no fueron visitados
+        # 3: Aumentar contador luego de cada DFS
+        self._reset_visited()
+        cc = []
+        for n in self.nodes:
+            # Si el nodo no fue visitado, realizar DFS 
+            if(not self.nodes[n].visited):
+                ## call dfs(n) and append the list
+                cc.append()  
+        return cc
+    
+    # Cantidad de componented conexos
+    def count_connected_components(self):
+        # 1: Resetear visitados
+        # 2: Realizar busqueda en profundidad (DFS) en aquellos nodos que no fueron visitados
+        # 3: Aumentar contador luego de cada DFS
+        self._reset_visited()
+        count = 0
+        for n in self.nodes:
+            # Si el nodo no fue visitado, realizar DFS 
+            if(not self.nodes[n].visited):
+                ## call dfs(n) and append the list
+                count += 1 
+        return count
+
+    # Es conexto
+    def is_connected(self):
+        # 1: Se hace una busqueda en profunidad
+        # 2: Se recorren todos los nodos y se pregunta si fueron visitados
+        # 3: Si alguno no fue visitado, devuleve Falso, de lo contratio Verdadero
+        self._reset_visited()
+        # self.dfs()
+        for n in self.nodes:
+            if(not self.nodes[n].visited):
+                return False
+        return True
 
     def _get_node(self, nodeValue):
         return self.nodes[nodeValue]
 
     def _lookup_node(self, nodeValue):
         return nodeValue in self.nodes
+    
+    def _reset_visited(self):
+        for n in self.nodes:
+            self.nodes[n].visited = False
